d43169a9ea206f1a4e133d5e7d532093936ab20f
remi-bezot <bezot.remi@gmail.com>
2026-01-29 09:46:07 +0100

    feat(products/varina/backend): add anyhow for improved error handling - Part of #34

--- projects/products/varina/backend/src/app.rs
+++ projects/products/varina/backend/src/app.rs
@@ -1,6 +1,6 @@
 //! projects/products/varina/backend/src/app.rs
 /// Example of simulating commands for debugging (under the `debug_cli` feature flag)
 /// This section was removed to align the architecture with the objectives.
-pub fn run_backend() -> Result<(), String> {
+pub fn run_backend() -> anyhow::Result<()> {
     Ok(())
 }
