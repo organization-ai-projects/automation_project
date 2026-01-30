f36d664328b829c974d0f68ef09d3b5c57865072
remi-bezot <bezot.remi@gmail.com>
2026-01-30 11:27:04 +0100

    refactor(libraries/common_time): extract fake clock state - Part of #66

--- projects/libraries/common_time/src/lib.rs
+++ projects/libraries/common_time/src/lib.rs
@@ -2,6 +2,7 @@
 pub mod backoff;
 pub mod clock;
 pub mod fake_clock;
+mod fake_clock_state;
 pub mod mono_instant;
 pub mod system_clock;
 pub mod time_span;
@@ -16,4 +17,6 @@ pub use mono_instant::MonoInstant;
 pub use system_clock::SystemClock;
 pub use time_span::TimeSpan;
 pub use timeout::with_timeout;
+pub use timestamp_utils::Timestamp;
+pub use timestamp_utils::current_timestamp_ms;
 pub use units::{FIFTEEN_MINUTES, ONE_DAY, ONE_HOUR, ONE_MINUTE, ONE_MONTH, ONE_WEEK};
