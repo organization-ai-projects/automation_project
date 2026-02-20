ad1a3368448588f18444641c30b4b64508681ae4
remi-bezot <bezot.remi@gmail.com>
2026-02-20 18:33:22 +0100

    refactor(projects/libraries/security): remove legacy security path after layers migration

--- projects/libraries/security/src/permission_error.rs
+++
@@ -1,8 +0,0 @@
-// projects/libraries/security/src/permission_error.rs
-use thiserror::Error;
-
-#[derive(Debug, Error, Clone)]
-pub enum PermissionError {
-    #[error("unauthorized: insufficient permissions")]
-    Unauthorized,
-}
--- projects/libraries/security/src/tests/mod.rs
+++
@@ -1,6 +0,0 @@
-mod helpers;
-mod password;
-mod permissions;
-mod role;
-mod token;
-mod token_service;
