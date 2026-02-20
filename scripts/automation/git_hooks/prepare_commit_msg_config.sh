9070bc0513d93084d2b1b2d0a949f9c259e7b023
remi-bezot <bezot.remi@gmail.com>
2026-02-20 09:49:06 +0100

    refactor(scripts/automation/git_hooks): remove redundant prepare-commit config wrapper
    Source shared scope resolver directly from prepare-commit-msg and delete the now-empty prepare_commit_msg_config wrapper.
    
    Part of #639

--- scripts/automation/git_hooks/prepare_commit_msg_config.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Shared scope resolver for prepare-commit-msg.
-
-# shellcheck source=scripts/automation/git_hooks/lib/scope_resolver.sh
-source "$(git rev-parse --show-toplevel)/scripts/automation/git_hooks/lib/scope_resolver.sh"
