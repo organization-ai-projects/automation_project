385e4ac652e5ebaa003a6e1214f1122802a63ffd
remi-bezot <bezot.remi@gmail.com>
2026-02-21 10:05:59 +0100

    refactor(projects/products/unstable/autonomous_dev_ai): flatten neural governance module layout

--- projects/products/unstable/autonomous_dev_ai/src/neural/mod.rs
+++ projects/products/unstable/autonomous_dev_ai/src/neural/mod.rs
@@ -1,10 +1,21 @@
 //! Neural computation layer - advisory only.
 
-pub mod governance;
+mod confidence_gate;
+mod drift_detector;
 mod intent_interpretation;
+mod model_governance;
+mod model_registry;
+mod model_version;
 mod neural_layer;
 mod neural_model;
+mod rollout_state;
 
+pub use confidence_gate::ConfidenceGate;
+pub use drift_detector::DriftDetector;
 pub use intent_interpretation::IntentInterpretation;
+pub use model_governance::ModelGovernance;
+pub use model_registry::ModelRegistry;
+pub use model_version::ModelVersion;
 pub use neural_layer::NeuralLayer;
 pub use neural_model::NeuralModel;
+pub use rollout_state::RolloutState;
