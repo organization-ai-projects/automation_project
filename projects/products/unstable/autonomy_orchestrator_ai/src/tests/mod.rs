82f53f9774a9c788a2e6657e007eb169e3dded2e
remi-bezot <bezot.remi@gmail.com>
2026-02-26 19:14:40 +0100

    test(projects/products/unstable/autonomy_orchestrator_ai): expand unit test coverage

--- projects/products/unstable/autonomy_orchestrator_ai/src/tests/mod.rs
+++ projects/products/unstable/autonomy_orchestrator_ai/src/tests/mod.rs
@@ -1,3 +1,11 @@
 // projects/products/unstable/autonomy_orchestrator_ai/src/tests/mod.rs
 #[cfg(test)]
+mod binary_runner;
+#[cfg(test)]
+mod config_runtime;
+#[cfg(test)]
 mod orchestrator;
+#[cfg(test)]
+mod run_args;
+#[cfg(test)]
+mod validation_invocation_parser;
