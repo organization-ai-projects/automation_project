11b51a4679f18aabfaa582151a7005afba698c13
remi-bezot <bezot.remi@gmail.com>
2026-03-01 09:37:51 +0100

    chore(projects/products/unstable/simkernel): stop tracking build artifacts under target

--- projects/products/unstable/simkernel/target/debug/deps/lazy_static-fe0ab9ac01c8b763.d
+++
@@ -1,8 +0,0 @@
-/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/deps/lazy_static-fe0ab9ac01c8b763.d: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/deps/liblazy_static-fe0ab9ac01c8b763.rlib: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/deps/liblazy_static-fe0ab9ac01c8b763.rmeta: /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs /home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs
-
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/lib.rs:
-/home/runner/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/lazy_static-1.5.0/src/inline_lazy.rs:
--- projects/products/unstable/simkernel/target/debug/deps/tooling_test-515bbce9d9ddaea0.d
+++
@@ -1,7 +0,0 @@
-/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/deps/tooling_test-515bbce9d9ddaea0.d: tooling/tests/tooling_test.rs
-
-/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/deps/tooling_test-515bbce9d9ddaea0: tooling/tests/tooling_test.rs
-
-tooling/tests/tooling_test.rs:
-
-# env-dep:CARGO_BIN_EXE_simkernel_tooling=/home/runner/work/automation_project/automation_project/projects/products/unstable/simkernel/target/debug/simkernel_tooling
