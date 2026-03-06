b85cead7b6fab2315440ce3189067a55af650ea2
remi-bezot <bezot.remi@gmail.com>
2026-03-06 21:48:00 +0100

    refactor(shell): reuse generic section replacement for validation gate
    Delegate validation gate body replacement to pr_replace_top_level_section_in_body to avoid duplicated awk replacement logic.

--- scripts/versioning/file_versioning/github/pr/validation_gate.sh
+++ scripts/versioning/file_versioning/github/pr/validation_gate.sh
@@ -37,49 +37,5 @@ pr_build_validation_gate_section() {
 pr_replace_validation_gate_in_body() {
   local original_body="$1"
   local replacement="$2"
-  local repl_file
-  repl_file="$(mktemp)"
-  printf "%s\n" "$replacement" >"$repl_file"
-
-  awk -v repl_file="$repl_file" '
-    function print_replacement() {
-      while ((getline line < repl_file) > 0) {
-        print line
-      }
-      close(repl_file)
-    }
-    BEGIN {
-      in_validation = 0
-      replaced = 0
-    }
-    {
-      if ($0 == "### Validation Gate") {
-        print_replacement()
-        replaced = 1
-        in_validation = 1
-        next
-      }
-
-      if (in_validation) {
-        if ($0 ~ /^### /) {
-          in_validation = 0
-          print ""
-          print $0
-        }
-        next
-      }
-
-      print $0
-    }
-    END {
-      if (!replaced) {
-        if (NR > 0) {
-          print ""
-        }
-        print_replacement()
-      }
-    }
-  ' <<<"$original_body"
-
-  rm -f "$repl_file"
+  pr_replace_top_level_section_in_body "$original_body" "### Validation Gate" "$replacement"
 }
