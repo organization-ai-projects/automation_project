b8ffa28383dde377b836d2337ece6aeb6ff86366
remi-bezot <bezot.remi@gmail.com>
2026-03-06 19:41:19 +0100

    refactor(shell): normalize pr module names and remove wrapper files

--- scripts/versioning/file_versioning/github/pr/pr_extraction.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck shell=bash
-# shellcheck disable=SC1091,SC2034,SC2154
-
-# Compatibility entrypoint; delegated to pr/extraction/module.sh.
-
-source "$(cd "${BASH_SOURCE[0]%/*}/extraction" && pwd)/module.sh"
--- scripts/versioning/file_versioning/github/pr/pr_pipeline.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# shellcheck shell=bash
-# shellcheck disable=SC1091,SC2034,SC2154
-
-# Compatibility entrypoint; delegated to pr/pipeline/module.sh.
-
-source "$(cd "${BASH_SOURCE[0]%/*}/pipeline" && pwd)/module.sh"
