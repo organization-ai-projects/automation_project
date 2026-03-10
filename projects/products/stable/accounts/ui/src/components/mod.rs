c7635bbdae6a03b01d611039d8dc318365e3372d
remi-bezot <bezot.remi@gmail.com>
2026-03-10 13:05:26 +0100

    fix(projects/products/stable/accounts/ui): expose component modules at crate scope
    Part of #1075

--- projects/products/stable/accounts/ui/src/components/mod.rs
+++ projects/products/stable/accounts/ui/src/components/mod.rs
@@ -1,5 +1,6 @@
-mod card_components;
-mod form_components;
+//! projects/products/stable/accounts/ui/src/components/mod.rs
+pub(crate) mod card_components;
+pub(crate) mod form_components;
 
 #[cfg(test)]
 mod tests;
