73ffba65837b2fb4be05ab4207e044c478a3c226
remi-bezot <bezot.remi@gmail.com>
2026-03-23 21:10:17 +0100

    refactor(projects/products/unstable/autonomous_dev_ai/backend): split old crate into backend and ui

--- projects/products/unstable/autonomous_dev_ai/backend/src/persistence/tests/mod.rs
+++ projects/products/unstable/autonomous_dev_ai/backend/src/persistence/tests/mod.rs
@@ -1,5 +1,5 @@
 //! projects/products/unstable/autonomous_dev_ai/backend/src/persistence/tests/mod.rs
-mod config_io;
-mod memory_store;
-mod memory_state_index;
 mod artifacts;
+mod config_io;
+mod memory_state_index;
+mod memory_store;
