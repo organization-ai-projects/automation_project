4867dc44257e3325e529bef2dee1ab3437945938
remi-bezot <bezot.remi@gmail.com>
2026-03-28 21:59:34 +0100

    --amend

--- tools/versioning_automation/src/lazy_regex.rs
+++ tools/versioning_automation/src/lazy_regex.rs
@@ -2,5 +2,5 @@ use regex::Regex;
 use std::sync::LazyLock;
 
 pub(crate) static PARENT_FIELD_REGEX: LazyLock<Result<Regex, regex::Error>> = LazyLock::new(|| {
-    Regex::new(r"(?i)^\s*Parent:\s*(#?[0-9]+|none|base|epic|\\(none\\)|\\(base\\)|\\(epic\\))\s*$")
+    Regex::new(r"(?i)^\s*Parent:\s*(#?[0-9]+|none|base|epic|\(none\)|\(base\)|\(epic\))\s*$")
 });
